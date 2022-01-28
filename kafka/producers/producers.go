9c1b7876e2154d45680573ed3252ced65d008e4e
Derek Chau <derek.chau@flipp.com>
2022-01-28 09:23:13 -0500

    First draft for generic consumers

--- kafka/producers/producers.go
+++ kafka/producers/producers.go
@@ -6,9 +6,13 @@ import (
 	"github.com/wishabi/kafka.go/avro"
 )
 
-// Should not have to provide the schema reg, kafka broker url
-// Or topic even. Should be able to get this via. helper function
-// Need a better way to store/do configs per topic
+/*
+	Duplicated code per producer AND consumer ... this is not ideal
+	Also Should not have to provide the schema reg, kafka broker url
+	Or even a topic.
+	Should be able to get this via. helper function
+	Need a better way to store/do configs per topic!
+*/
 func GetFsaProducer(schemaRegistry *avro.Registry, dialer *kafka.Dialer, kafkaBrokerUrl string) *producers.AvroFsaProducer {
 	var fsaProducer = producers.NewAvroFsaProducer(kafka.WriterConfig{
 		Topic:  "Fsa",
@@ -18,4 +22,15 @@ func GetFsaProducer(schemaRegistry *avro.Registry, dialer *kafka.Dialer, kafkaBr
 		},
 	}, schemaRegistry)
 	return fsaProducer
+}
+
+func GetStateProducer(schemaRegistry *avro.Registry, dialer *kafka.Dialer, kafkaBrokerUrl string) *producers.AvroStateProducer {
+	var stateProducer = producers.NewAvroStateProducer(kafka.WriterConfig{
+		Topic:  "State",
+		Dialer: dialer, // just add the dialer here
+		Brokers: []string{
+			kafkaBrokerUrl,
+		},
+	}, schemaRegistry)
+	return stateProducer
 }
--- main.go
+++ main.go
@@ -8,8 +8,13 @@ import (
 	"time"
 
 	"github.com/DeeChau/kafka.go-generic/internal/schema"
+	"github.com/DeeChau/kafka.go-generic/internal/generic_consumer"
+	// "github.com/DeeChau/kafka.go-generic/internal/generic_schema"
+
+	// schema "github.com/DeeChau/kafka.go-generic/internal/schema"
 	"github.com/DeeChau/kafka.go-generic/kafka/consumers"
 	"github.com/DeeChau/kafka.go-generic/kafka/producers"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/segmentio/kafka-go"
 	"github.com/wishabi/kafka.go/avro"
@@ -18,20 +23,132 @@ import (
 )
 
 func main() {
-	fmt.Println("---Begin Hackathon for Generic Kafka Consumers---")
+	fmt.Println("---Begin Hackathon for Generic Kafka experimentation---")
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
-	useGenerics := os.Args[1] == "generic"
+	useGenerics := len(os.Args) >= 2 && (os.Args[1] == "generic")
 
 	if useGenerics {
-		return
+		experimentWithGenerics()
+		consumeWithGenerics()
 	} else {
 		publishAndConsumeNoGenerics()
 	}
 }
 
-// Not using generics!!!
+// Using generics
+func consumeWithGenerics() (bool, error) {
+	fmt.Println("---Using Generic Kafka Consumers/Producers---")
+	// Begin Kafka code here
+	dialer, dialerErr := kafkaUtil.Dialer()
+
+	if dialerErr != nil {
+		log.Error().Msg("Error getting kafka dialer.")
+		return false, dialerErr
+	}
+
+	schemaRegistry := avro.NewRegistry(env.SchemaRegistry, &http.Client{})
+
+
+	// fsa Consumer code
+	fsaConsumer := genericconsumers.NewAvroConsumer[schema.FsaKey, schema.Fsa](
+		kafka.ReaderConfig{
+			Topic:       "Fsa",
+			GroupID:     "TestFsaConsumerGenerics",
+			Dialer:      dialer,
+			Brokers: env.BrokersList,
+	}, schemaRegistry)
+
+	log.Info().Msgf("Fsa Consumer Initialized: %s\n", fsaConsumer)
+
+	ctx := context.Background()
+	fsaMsg, fsaErr := fsaConsumer.AutoCommitConsume(ctx)
+	if fsaErr != nil {
+		log.Error().Err(fsaErr).Msg("Kafka message could not be received. Error consuming message.")
+		return false, fsaErr
+	}
+
+	fsaKey := fsaMsg.Key
+	fsaValue := fsaMsg.Value
+	log.Info().Msgf("Fsa -- Kafka: Received Key %v, Message %v from", fsaKey, fsaValue)
+
+	// State Consumer code
+	stateConsumer := genericconsumers.NewAvroConsumer[schema.StateKey, schema.State](
+		kafka.ReaderConfig{
+			Topic:       "State",
+			GroupID:     "TestFsaConsumerGenerics",
+			Dialer:      dialer,
+			Brokers: env.BrokersList,
+	}, schemaRegistry)
+
+	log.Info().Msgf("State Consumer Initialized: %s\n", stateConsumer)
+
+	ctx = context.Background()
+	stateMsg, stateErr := stateConsumer.AutoCommitConsume(ctx)
+	if stateErr != nil {
+		log.Error().Err(stateErr).Msg("Kafka message could not be received. Error consuming message.")
+		return false, stateErr
+	}
+
+	stateKey := stateMsg.Key
+	stateValue := stateMsg.Value
+	log.Info().Msgf("State -- Kafka: Received Key %v, Message %v from", stateKey, stateValue)
+
+	return true, nil
+}
+
+func experimentWithGenerics() {
+	fmt.Println("---Experimenting with Generic Types and Methods---")
+	timestamp := time.Now()
+
+	fsa_key := schema.FsaKey{Label: "M5V"}
+	fsa_value := schema.Fsa{
+		Label: "M5V",
+		Latitude: 12.34,
+		Longitude: 56.78,
+		Message_id: timestamp.Format(time.UnixDate),
+		Created_at: timestamp.Unix(),
+		Updated_at: timestamp.Unix(),
+		Timestamp:  timestamp.Format(time.UnixDate),
+	}
+	state_key := schema.StateKey{Abbreviation: "ON"}
+	state_value := schema.State{
+		Abbreviation: "ON",
+		Name: "Ontario",
+		Message_id: timestamp.Format(time.UnixDate),
+		Timestamp:  timestamp.Format(time.UnixDate),
+	}
+
+	printAvroKey(fsa_key)
+	printAvroKey(state_key)
+	printAvroValue(fsa_value)
+	printAvroValue(state_value)
+}
+
+// With this pattern, we can use this as a building block to define constraints on what a Avro Key or Value should be
+// - and have it generated to redude duplication of code.
+// "Hack"(?) that allows us to call methods
+func printAvroKey[K any, PT schema.AvroSchemaStruct[K]](key K) {
+	fmt.Printf("%T Key: %v\n", key, key)
+	fmt.Printf("Key Schema: %s\n\n", PT(&key).Schema())
+}
+
+func printAvroValue[V any, PT schema.AvroSchemaStruct[V]](value V) {
+	fmt.Printf("%T Value %v\n", value, value)
+	fmt.Printf("Value Schema: %s\n\n", PT(&value).Schema())
+}
+
+// Not using generics
 func publishAndConsumeNoGenerics() {
+	fmt.Println("---Not using Generic Kafka Consumers/Producers---")
 	_, err := publishFsa("M5V", 43.6450946, -79.3929024)
+	if err != nil {
+		log.Info().Msg("Some error occurred while producing to Kafka")
+	} else {
+		log.Info().Msg("Producers successfully finished")
+	}
+
+	_, err = publishState("ON", "Ontario")
 	if err != nil {
 		log.Info().Msg("Some error occurred while producing to Kafka")
 	} else {
@@ -47,6 +164,8 @@ func publishAndConsumeNoGenerics() {
 }
 
 // Consumer code.
+// Currently not being run with too much - in the future we need to
+// investigate a platform for this to run within a GoRoutine.
 func consumeFsas() (bool, error) {
 	log.Info().Msg("Create consumers")
 
@@ -57,7 +176,6 @@ func consumeFsas() (bool, error) {
 		return false, dialerErr
 	}
 
-	// dialer, dialerErr := kafkaUtil.Dialer()
 	schemaRegistry := avro.NewRegistry(env.SchemaRegistry, &http.Client{})
 
 	consumer := consumers.GetFsaConsumer(schemaRegistry, dialer, env.BrokersList[0])
@@ -70,7 +188,7 @@ func consumeFsas() (bool, error) {
 
 	key := msg.Key
 	value := msg.Value
-	log.Printf("---> Kafka: Received Key %v, Message %v from", key, value)
+	log.Info().Msgf("Kafka: Received Key %v, Message %v", key, value)
 
 	return true, nil
 }
@@ -89,7 +207,7 @@ func publishFsa(fsa string, lat, long float32) (bool, error) {
 	key := generateFsaKey(fsa)
 	payload := generateFsa(fsa, lat, long)
 
-	log.Printf("Key to send %v, Message to send %v", key, payload)
+	log.Info().Msgf("Key to send %v, Message to send %v", key, payload)
 	log.Printf("Schema Registry location %v, Kafka Broker location %v", env.SchemaRegistry, env.BrokersList)
 
 	err := produceFsaMessage(dialer, &key, &payload)
@@ -102,13 +220,47 @@ func publishFsa(fsa string, lat, long float32) (bool, error) {
 	return true, nil
 }
 
-// These should probably be abstracted and made easier, possible within the producer's library.
+func publishState(abbreviation, name string) (bool, error) {
+	log.Info().Msg("Create Producers")
+
+	dialer, dialerErr := kafkaUtil.Dialer()
+
+	if dialerErr != nil {
+		log.Error().Msg("Error getting kafka dialer.")
+		return false, dialerErr
+	}
+
+	key := generateStateKey(abbreviation)
+	payload := generateState(abbreviation, name)
+
+	log.Info().Msgf("Key to send %v, Message to send %v", key, payload)
+	log.Printf("Schema Registry location %v, Kafka Broker location %v", env.SchemaRegistry, env.BrokersList)
+
+	err := produceStateMessage(dialer, &key, &payload)
+	if err != nil {
+		log.Error().Err(err).Msg("Kafka message could not be sent. Error producing message.")
+		return false, err
+	}
+
+	log.Info().Msg("State kafka message sent successfully.")
+	return true, nil
+}
+
+
+/* Notes
+Implementation & Use 	- These should probably be abstracted and made easier - it should be possible within the producer's library.
+Linting 			 	- These keys should be standardized with Camel Case or snake case - not both.
+Implementation & Use 	- Utilize Type Switch to take in a Schema Type -> Translate to the Configured Kafka Topic ID - PENDING on discussions from golang maintainers
+Implementation & Use 	- Share one registry across? Is that a good idea.
+Implementation & Use 	- Utilize Generics
+*/
+// - Begin FSA Producer helper code - //
 func generateFsaKey(fsa string) schema.FsaKey {
 	return schema.FsaKey{Label: fsa}
 }
 
 func generateFsa(fsa string, lat, long float32) schema.Fsa {
-	// These keys should be standardized with Camel Case or snake case - not both.
+	
 	timestamp := time.Now()
 	payload := schema.Fsa{
 		Label:      fsa,
@@ -123,11 +275,35 @@ func generateFsa(fsa string, lat, long float32) schema.Fsa {
 	return payload
 }
 
-// Utilize Type Switch to take in a Schema Type -> Translate to the Configured Kafka Topic ID.
 func produceFsaMessage(dialer *kafka.Dialer, key *schema.FsaKey, payload *schema.Fsa) error {
-	// Share one registry across? Is that a good idea.
+	
 	schemaRegistry := avro.NewRegistry(env.SchemaRegistry, &http.Client{})
 	return producers.GetFsaProducer(schemaRegistry, dialer, env.BrokersList[0]).Produce(
+		context.Background(),
+		key,
+		payload)
+}
+
+// - Begin State Producer helper code - //
+func generateStateKey(abbreviation string) schema.StateKey {
+	return schema.StateKey{Abbreviation: abbreviation}
+}
+
+func generateState(abbreviation, name string) schema.State {
+	timestamp := time.Now()
+	payload := schema.State{
+		Abbreviation: abbreviation,
+		Name:   name,
+		Message_id: fmt.Sprintf("State-%v", timestamp),
+		Timestamp:  timestamp.Format(time.UnixDate),
+	}
+
+	return payload
+}
+
+func produceStateMessage(dialer *kafka.Dialer, key *schema.StateKey, payload *schema.State) error {
+	schemaRegistry := avro.NewRegistry(env.SchemaRegistry, &http.Client{})
+	return producers.GetStateProducer(schemaRegistry, dialer, env.BrokersList[0]).Produce(
 		context.Background(),
 		key,
 		payload)
